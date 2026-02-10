149721f90a97286a193be729f22385c3c05efcf5
techankit09 <ankitrawat0988@gmail.com>
2026-02-10 13:49:49 +0530

    done

---
+++ spring-dao-hibernate/src/main/java/com/rays/dao/UserDAOHibImpl.java
@@ -0,0 +1,85 @@
+package com.rays.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.rays.dto.UserDTO;
+@Repository
+public class UserDAOHibImpl implements UserDAOInt {
+	
+	@Autowired
+	private SessionFactory sessionfactory = null;
+
+	public long add(UserDTO dto) {
+		Session session = sessionfactory.getCurrentSession();
+		long pk = (Long) session.save(dto);
+		return pk;
+	}
+
+	public void update(UserDTO dto) {
+		sessionfactory.getCurrentSession().update(dto);
+	}
+
+	public void delete(long pk) {
+		Session session = sessionfactory.getCurrentSession();
+		UserDTO dto = findByPk(pk);
+		session.delete(dto);
+	}
+
+	public UserDTO findByPk(long pk) {
+		Session session = sessionfactory.getCurrentSession();
+		UserDTO dto = session.get(UserDTO.class, pk);
+		return dto;
+	}
+
+	public UserDTO findByLogin(String login) {
+		Session session = sessionfactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(UserDTO.class);
+		criteria.add(Restrictions.eq("login", login));
+
+		List list = criteria.list();
+
+		UserDTO dto = null;
+
+		if (list.size() == 1) {
+			dto = (UserDTO) list.get(0);
+		}
+
+		return dto;
+	}
+
+	public UserDTO authenticate(String login, String password) {
+		
+		return null;
+	}
+
+	public List<UserDTO> search(UserDTO dto, int pageNo, int pageSize) {
+		List<UserDTO> list = null;
+
+		Session session = sessionfactory.getCurrentSession();
+
+		Criteria criteria = session.createCriteria(UserDTO.class);
+
+		if (dto != null) {
+			if (dto.getFirstName() != null && dto.getFirstName().length() > 0) {
+				criteria.add(Restrictions.like("firstName", dto.getFirstName() + "%"));
+			}
+		}
+
+		if (pageSize > 0) {
+			pageNo = (pageNo - 1) * pageSize;
+			criteria.setFirstResult(pageNo);
+			criteria.setMaxResults(pageSize);
+		}
+
+		list = criteria.list();
+
+		return list;
+	}
+}
---
+++ spring-dao-hibernate/src/main/java/com/rays/test/TestUserService.java
@@ -0,0 +1,107 @@
+package com.rays.test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.rays.dto.UserDTO;
+import com.rays.service.UserService;
+
+@Component("testUserService")
+public class TestUserService {
+	
+	@Autowired
+	public UserService service = null;
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		 TestUserService test = (TestUserService) context.getBean("testUserService");
+		 
+
+//			test.testAdd();
+//			test.testUpdate();
+//			test.testFindByPk();
+//		    test.testDelete();
+			test.testFindByLogin();
+//		    test.testSearch();
+//			test.testAuthenticate();
+	}
+	
+	public void testAdd() {
+		UserDTO dto = new UserDTO();
+		dto.setFirstName("Danny");
+		dto.setLastname("D");
+		dto.setLogin("Danny@gmail.com");
+		dto.setPassword("Danny123");
+		long pk = service.add(dto);
+		System.out.println("PK----> " + pk);
+	}
+	
+	private void testSearch() {
+		UserDTO dto = new UserDTO();
+		  dto.setFirstName("Danny");
+		  int pageNo = 1;
+		  int pageSize = 5;
+		  List<UserDTO> list = service.search(dto, pageNo, pageSize);
+		  
+		  Iterator<UserDTO> it = list.iterator();
+		   while(it.hasNext()) {
+			   dto = it.next();
+			   System.out.println(dto.getId());
+			   System.out.println(dto.getFirstName());
+			   System.out.println(dto.getLastname());
+			   System.out.println(dto.getLogin());
+			   System.out.println(dto.getPassword());
+		   }
+	}
+    
+	private void testFindByLogin() {
+		UserDTO dto = new UserDTO();
+		dto = service.findByLogin("Danny@gmail.com");
+		if (dto != null) {
+			System.out.println(dto.getId());
+			System.out.println(dto.getFirstName());
+			System.out.println(dto.getLastname());
+			System.out.println(dto.getLogin());
+			System.out.println(dto.getPassword());
+		} else {
+			System.out.println("user not found");
+	}
+  }
+	private void testUpdate() {
+		UserDTO dto = new UserDTO();
+		dto.setId(1);
+		dto.setFirstName("Kamal");
+		dto.setLastname("Yadav");
+		dto.setLogin("ram@gmail.com");
+		dto.setPassword("ram123");
+		service.update(dto);
+	}
+
+	private void testFindByPk() {
+		UserDTO dto = service.findByPk(2);
+		System.out.println(dto.getId());
+		System.out.println(dto.getFirstName());
+	}
+
+	private void testDelete() {
+		service.delete(2);
+	}
+
+	private void testAuthenticate() {
+		UserDTO dto = service.authenticate("ram@gmail.com", "ram123");
+		System.out.println(dto.getId());
+		System.out.println(dto.getFirstName());
+		System.out.println(dto.getLastname());
+		System.out.println(dto.getLogin());
+		System.out.println(dto.getPassword());
+	}
+}
+	
+
